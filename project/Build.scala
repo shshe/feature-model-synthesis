9afab77226f9d8684cacd0d8e62714c5aa26254c
Steven She <shshe@gsd.uwaterloo.ca>
2012-10-15 18:36:58 -0400

    updated build for evaluaiton

--- project/Build.scala
+++ project/Build.scala
@@ -4,5 +4,7 @@ object FMSBuild extends Build {
     lazy val graph = Project(id = "graph", base = file("graph/"))
     lazy val graphCompat = Project(id = "graph-compat", base = file("graph-compat/")) dependsOn (graph)
     lazy val root = Project(id = "feature-model-synthesis", base = file(".")) dependsOn (graph, graphCompat)
+
+    lazy val evaluation = Project(id = "evaluation", base = file("evaluation/")) dependsOn (root) 
 }
 
