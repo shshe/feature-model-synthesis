bca190aa5926ed900ece39b39f8f76a05821e0aa
Steven She <shshe@gsd.uwaterloo.ca>
2012-10-22 17:00:35 -0400

    dnf test case

--- src/test/scala/gsd/fms/dnf/DNFImplBuilderTest.scala
+++ src/test/scala/gsd/fms/dnf/DNFImplBuilderTest.scala
@@ -4,23 +4,23 @@ import org.scalatest.FunSuite
 
 class DNFImplBuilderTest extends FunSuite {
 
-  test ("empty builder is initialized") {
-    new DNFImplBuilder(List(),1)
+  test("empty builder is initialized") {
+    new DNFImplBuilder(List(), 1)
   }
 
-  test ("implication is found (1)") {
-    val b1 = new DNFImplBuilder(List(List(-1,2)),2)
-    assert(b1.implication(1,2))
+  test("implication is found (1)") {
+    val b1 = new DNFImplBuilder(List(List(-1, 2)), 2)
+    assert(b1.implication(1, 2))
   }
 
-  test ("implication is found (2)") {
-    val b2 = new DNFImplBuilder(List(List(-1,2), List(-1,2,3)),3)
-    assert(b2.implication(1,2))
+  test("implication is found (2)") {
+    val b2 = new DNFImplBuilder(List(List(-1, 2), List(-1, 2, 3)), 3)
+    assert(b2.implication(1, 2))
   }
 
-  test ("invalid implication is not found") {
-    val b = new DNFImplBuilder(List(List(-1,2), List(2)), 2)
-    assert(!b.implication(1,2))
+  test("invalid implication is not found") {
+    val b = new DNFImplBuilder(List(List(-1, 2), List(2)), 2)
+    assert(!b.implication(1, 2))
   }
 
 }
